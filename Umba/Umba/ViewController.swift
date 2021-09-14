39c544a82286b4ae532ad97a84d1cd5bedb384cd
ibarra2521 <ibarra2521@gmail.com>
2021-09-13 19:31:39 -0500

    Init

---
+++ Umba/Umba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Umba
+//
+//  Created by Nivardo Ibarra on 9/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
